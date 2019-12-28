356152a01422609e736af68a024470a64ec5b622
Nojus Gudinavičius <nojus.gudinavicius@gmail.com>
2019-12-28 18:07:23 +0200

    Finish Eval function. Now actually possible to download

--- cli/cli.go
+++ cli/cli.go
@@ -1,9 +1,11 @@
 package cli
 
 import (
+	"errors"
 	"github.com/Nojus297/wedl/transfer"
-	// "github.com/cheggaaa/pb"
-	// "fmt"
+	"github.com/cheggaaa/pb"
+	"io"
+	"os"
 )
 
 func Eval(opts map[string]interface{}) (err error) {
@@ -11,14 +13,27 @@ func Eval(opts map[string]interface{}) (err error) {
 	if err != nil {
 		return
 	}
-	// dl, err := transfer.GetDownloadResponse(parsed.Url)
-	// if err != nil {
-	// 	return
-	// }
-	_, err = transfer.GetWriter(parsed.Output, parsed.Path, parsed.Force)
+	resp, err := transfer.GetDownloadResponse(parsed.Url)
+	defer resp.Body.Close()
 	if err != nil {
 		return
 	}
-
+	if parsed.Output == "" {
+		parsed.Output = transfer.FilenameFromUrl(resp.Request.URL.String())
+	}
+	if parsed.Output == "" {
+		return errors.New("Canot find filename")
+	}
+	writer, err := transfer.GetWriter(parsed.Output, parsed.Path, parsed.Force)
+	if err != nil {
+		return
+	}
+	if !parsed.Silent {
+		bar := pb.New64(resp.ContentLength).SetUnits(pb.U_BYTES_DEC)
+		bar.Output = os.Stderr
+		bar.Start()
+		writer = bar.NewProxyWriter(writer)
+	}
+	io.Copy(writer, resp.Body)
 	return
 }
