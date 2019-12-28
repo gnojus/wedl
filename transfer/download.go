41ce39fb51ef188f56c90945b1c72d7544be6405
Nojus Gudinavičius <nojus.gudinavicius@gmail.com>
2019-12-28 14:18:27 +0200

    Add transfer data parsing

--- transfer/download.go
+++ transfer/download.go
@@ -3,9 +3,24 @@ package transfer
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"io"
+	"io/ioutil"
 	"net/http"
+	"regexp"
 )
+
+type requestData struct {
+	security_hash  string
+	domain_user_id string
+}
+
+type transferData struct {
+	transfer_id string
+	csrf_token  string
+	wt_session  string
+	data        requestData
+}
 
 func GetDownloadResponse(URL string) (out *io.ReadCloser, err error) {
 	client := &http.Client{}
@@ -13,7 +28,50 @@ func GetDownloadResponse(URL string) (out *io.ReadCloser, err error) {
 	if err != nil {
 		return
 	}
+	return
+}
 
+func getTransferData(resp *http.Response) (out transferData, err error) {
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	var ok bool
+	if out.transfer_id, ok = findVar(`var _preloaded_transfer_ = {"id":"`, body); !ok {
+		return out, errors.New("Unable to get transfer id")
+	}
+	if out.csrf_token, ok = findVar(`<meta name="csrf-token" content="`, body); !ok {
+		return out, errors.New("Unable to get csrf token")
+	}
+	if out.data.security_hash, ok = findVar(`"security_hash":"`, body); !ok {
+		return out, errors.New("Unable to get security hash")
+	}
+	if out.data.domain_user_id, ok = findVar(`user: {"key":"`, body); !ok {
+		return out, errors.New("Unable to get domain user id")
+	}
+	if out.wt_session, ok = getCookieValue("_wt_session", resp); !ok {
+		return out, errors.New("Unable to get _wt_session cookie")
+	}
+	return
+}
+
+func getCookieValue(name string, resp *http.Response) (out string, exists bool) {
+	for _, cookie := range resp.Cookies() {
+		if cookie.Name == name {
+			return cookie.Value, true
+		}
+	}
+	return
+}
+
+func findVar(prefix string, body []byte) (out string, exists bool) {
+	reg := regexp.MustCompile(prefix + `(.+)"`)
+	tmp := reg.FindSubmatch(body)
+	if tmp != nil {
+		out = string(tmp[1])
+		exists = true
+	}
 	return
 }
 
