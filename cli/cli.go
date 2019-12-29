08e37b4ea7dc70c8baf896e727c5dfeb794e4466
Nojus Gudinavičius <nojus.gudinavicius@gmail.com>
2019-12-29 15:39:47 +0200

    Print download destination and fix progressbar

--- cli/cli.go
+++ cli/cli.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"errors"
+	"fmt"
 	"github.com/Nojus297/wedl/transfer"
 	"github.com/cheggaaa/pb"
 	"io"
@@ -22,18 +23,26 @@ func Eval(opts map[string]interface{}) (err error) {
 		parsed.Output = transfer.FilenameFromUrl(resp.Request.URL.String())
 	}
 	if parsed.Output == "" {
-		return errors.New("Canot find filename")
+		return errors.New("Canot find any filename")
 	}
 	writer, err := transfer.GetWriter(parsed.Output, parsed.Path, parsed.Force)
 	if err != nil {
 		return
 	}
 	if !parsed.Silent {
+		output := parsed.Output
+		if output == "-" {
+			output = "stdout"
+		}
+		fmt.Fprintf(os.Stderr, "Writing to %s\n", output)
 		bar := pb.New64(resp.ContentLength).SetUnits(pb.U_BYTES_DEC)
 		bar.Output = os.Stderr
 		bar.Start()
 		writer = bar.NewProxyWriter(writer)
+		io.Copy(writer, resp.Body)
+		bar.Finish()
+	} else {
+		io.Copy(writer, resp.Body)
 	}
-	io.Copy(writer, resp.Body)
 	return
 }
